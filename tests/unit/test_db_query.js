9ec57beb72b9585fe01b6b14dec757aee37fd90b
Elzair <elzairthesorcerer@gmail.com>
2014-05-02 09:39:56 -0400

    Added test for last_name query

--- tests/unit/test_db_query.js
+++ tests/unit/test_db_query.js
@@ -13,7 +13,12 @@ module.exports = {
   , test_query_first_name: function(test) {
       var results = db.query({first_name: 'John'});
       var data = [test_data[0]];
-      console.log(data);
+      test_objects(test, results, data);
+      test.done();
+    }
+  , test_query_last_name: function(test) {
+      var results = db.query({last_name: 'Doe'});
+      var data = test_data.slice(0, 2);
       test_objects(test, results, data);
       test.done();
     }
